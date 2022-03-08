bdf4c12abf9917e1e474f57d08d9e366f67d52bd
Jannie87 <jannie.backman@gmail.com>
2022-03-08 16:00:26 +0100

    added all mocked products to productpage

--- src/components/Products/ProductsPage.tsx
+++ src/components/Products/ProductsPage.tsx
@@ -1,53 +1,32 @@
-import {Link} from "react-router-dom";
-import React, {CSSProperties} from "react";
+import { Link } from "react-router-dom";
+import React, { CSSProperties } from "react";
 import ChocoCard from "./chocoCard";
-import {mockedProducts} from "../../products";
+import { mockedProducts } from "../../products";
 
 export default function Chocolates() {
-    return (
-        <div style={productsContainer}>
-            <h3 style={productPageTitle}>Products</h3>
-            <div style={productPageCards}>
-
-                <div>
-               <ChocoCard product={mockedProducts[4]}/>
-
-                <ChocoCard product={mockedProducts[4]} />
-
-                <ChocoCard product={mockedProducts[4]} />
-
-                <ChocoCard product={mockedProducts[4]} />
-                </div>
-                <div>
-                <ChocoCard product={mockedProducts[4]} />
-
-                <ChocoCard product={mockedProducts[4]} />
-
-                <ChocoCard product={mockedProducts[4]} />
-
-                <ChocoCard product={mockedProducts[4]} />
-                </div>
-                </div>
-
-        </div>
-    )
+  return (
+    <div style={productsContainer}>
+      <h3 style={productPageTitle}>Products</h3>
+      <div style={productPageCards}>
+        {mockedProducts.map((product) => (
+          <ChocoCard product={product} />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 const productsContainer: CSSProperties = {
-    display: "flex",
-    flexDirection:"column"
-}
+  display: "flex",
+  flexDirection: "column",
+};
 
-const productPageTitle:CSSProperties = {
-    textAlign: "center"
-}
+const productPageTitle: CSSProperties = {
+  textAlign: "center",
+};
 
 const productPageCards: CSSProperties = {
-    display: "flex",
-    justifyContent: "center",
-    flexWrap: "wrap"
-}
-
-
-
-
+  display: "flex",
+  justifyContent: "center",
+  flexWrap: "wrap",
+};
