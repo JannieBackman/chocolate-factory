dba64ddb80233ea51b6f3c8bd68ab7921c493fbf
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-03-08 14:38:39 +0100

    created amountcounter component

---
+++ src/components/AmountCounter.tsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+import "./AmountCounter.css";
+import Cart from "./cart";
+
+function AmountCounter() {
+  const [count, setCount] = useState(0);
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleDecrement = () => {
+    if (count > 0) {
+      setCount(count - 1);
+    }
+  };
+
+  return (
+    <div className="container">
+      <button className="button" onClick={handleIncrement}>
+        +
+      </button>
+      <p className="amount">{count}</p>
+      <button className="button decrement-button" onClick={handleDecrement}>
+        -
+      </button>
+      <Cart />
+    </div>
+  );
+}
+
+export default AmountCounter;
