a794094be89fc893b9193b5ca25300384ccd8cb3
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-03-09 10:51:11 +0100

    created shipping options component

--- src/components/Cart/PaymentBasket.tsx
+++ src/components/Cart/PaymentBasket.tsx
@@ -1,65 +1,66 @@
-import React, {CSSProperties} from "react";
+import React, { CSSProperties } from "react";
 import { Form, Row, Col, Button } from "react-bootstrap";
+import ShippingOptions from "./ShippingOptions";
 
 export default function PaymentBasket() {
-    return (
-        <div>
-            <div>
-                <p style={cartStyle}>Your contact information</p>
+  return (
+    <div>
+      <div>
+        <p style={cartStyle}>Your contact information</p>
 
-                <Form>
-                    <Row className="mb-3">
-                        <Form.Group as={Col} controlId="formGridName">
-                            <Form.Label>Firstname</Form.Label>
-                            <Form.Control type="name" placeholder="Firstname" />
-                        </Form.Group>
+        <Form>
+          <Row className="mb-3">
+            <Form.Group as={Col} controlId="formGridName">
+              <Form.Label>Firstname</Form.Label>
+              <Form.Control type="name" placeholder="Firstname" />
+            </Form.Group>
 
-                        <Form.Group as={Col} controlId="formGridLastname">
-                            <Form.Label>Lastname</Form.Label>
-                            <Form.Control type="lastname" placeholder="Lastname" />
-                        </Form.Group>
-                    </Row>
+            <Form.Group as={Col} controlId="formGridLastname">
+              <Form.Label>Lastname</Form.Label>
+              <Form.Control type="lastname" placeholder="Lastname" />
+            </Form.Group>
+          </Row>
 
-                    <Form.Group className="mb-3" controlId="formGridAddress">
-                        <Form.Label>Address</Form.Label>
-                        <Form.Control placeholder="Address" />
-                    </Form.Group>
+          <Form.Group className="mb-3" controlId="formGridAddress">
+            <Form.Label>Address</Form.Label>
+            <Form.Control placeholder="Address" />
+          </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formGridCity">
-                        <Form.Label>City</Form.Label>
-                        <Form.Control placeholder="City" />
-                    </Form.Group>
+          <Form.Group className="mb-3" controlId="formGridCity">
+            <Form.Label>City</Form.Label>
+            <Form.Control placeholder="City" />
+          </Form.Group>
 
-                        <Form.Group as={Col} controlId="formGridZip">
-                            <Form.Label>Zip</Form.Label>
-                            <Form.Control placeholder="Zip" />
-                        </Form.Group>
+          <Form.Group as={Col} controlId="formGridZip">
+            <Form.Label>Zip</Form.Label>
+            <Form.Control placeholder="Zip" />
+          </Form.Group>
 
-                    <Row>
-                        <Form.Group className="mb-3" controlId="formGridPhone">
-                            <Form.Label>Phone number</Form.Label>
-                            <Form.Control placeholder="Phone number" />
-                        </Form.Group>
+          <Row>
+            <Form.Group className="mb-3" controlId="formGridPhone">
+              <Form.Label>Phone number</Form.Label>
+              <Form.Control placeholder="Phone number" />
+            </Form.Group>
 
-                        <Form.Group className="mb-3" controlId="formGridEmail">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control placeholder="Email" />
-                        </Form.Group>
-                    </Row>
+            <Form.Group className="mb-3" controlId="formGridEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control placeholder="Email" />
+            </Form.Group>
+          </Row>
 
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
 
-                <p style={cartStyle}>Choice payment</p>
-                <p style={cartStyle}>Choice shipping</p>
-            </div>
-        </div>
-    )
+        <p style={cartStyle}>Choice payment</p>
+        <p style={cartStyle}>Shipping options</p>
+        <ShippingOptions />
+      </div>
+    </div>
+  );
 }
 
-const cartStyle:CSSProperties = {
-    fontSize: "2rem",
-
-}
+const cartStyle: CSSProperties = {
+  fontSize: "2rem",
+};
---
+++ src/components/Cart/ShippingOptions.tsx
@@ -0,0 +1,39 @@
+import "./ShippingOptions.css";
+import Postnord from "../../assets/postnord-logo.png";
+import DHL from "../../assets/DHL-logo.png";
+import Bring from "../../assets/bring-logo.png";
+
+function ShippingOptions() {
+  return (
+    <div className="shipping-container">
+      <div className="postnord-container">
+        <input type="radio" value="Postnord" name="shipping" />
+        <img className="img-styling" src={Postnord} alt="" />
+        <p className="shipping-info">
+          48h shipping time. Expected delivery on March 11.
+        </p>
+        <p className="shipping-info">
+          Shipping fee: 25sek. Free if you shop for over 100sek.
+        </p>
+      </div>
+      <div className="dhl-container">
+        <input type="radio" value="DHL" name="shipping" />
+        <img className="img-styling" src={DHL} alt="" />
+        <p className="shipping-info">
+          72h shipping time. Expected delivery on March 12.
+        </p>
+        <p className="shipping-info">Free shipping!</p>
+      </div>
+      <div className="bring-container">
+        <input type="radio" value="Bring" name="shipping" />
+        <img className="img-styling" src={Bring} alt="" />
+        <p className="shipping-info">
+          24h shipping time. Expected delivery March 10.
+        </p>
+        <p className="shipping-info">Shipping fee: 40sek.</p>
+      </div>
+    </div>
+  );
+}
+
+export default ShippingOptions;
