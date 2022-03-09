136a07908ea7e1c3d203d53aeafdb725f3fd5dc8
Jannie87 <jannie.backman@gmail.com>
2022-03-09 13:01:20 +0100

    set the same fontColor

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,6 +1,7 @@
 import { CSSProperties } from "react";
 import Cart from "../Cart/cart";
 import { Link } from "react-router-dom";
+import NavigationBar from "./Navbar";
 
 function Header() {
   return (
@@ -19,20 +20,21 @@ function Header() {
 
 const rootStyle: CSSProperties = {
   display: "flex",
-  justifyContent: "center",
+  justifyContent: "space-around",
+  alignItems: "center",
   backgroundColor: "#333",
   color: "whitesmoke",
   height: "7rem",
 };
 
 const linkStyle: CSSProperties = {
-  color: "white",
+  color: "whitesmoke",
   fontSize: "3rem",
   textDecoration: "none",
 };
 
 const cartStyle: CSSProperties = {
   fontSize: "2rem",
-  color: "white",
+  color: "whitesmoke",
 };
 export default Header;
