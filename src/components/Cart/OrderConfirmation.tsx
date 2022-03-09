a2e1f9ff981febb7e34908ba8cc92141c15bb229
Anna Özmehak <90522443+A-Ozmehak@users.noreply.github.com>
2022-03-09 11:37:01 +0100

    contact information form

---
+++ src/components/Cart/OrderConfirmation.tsx
@@ -0,0 +1,8 @@
+
+export default function OrderConfirmation() {
+    return (
+        <div>
+
+        </div>
+    )
+}
--- src/components/Cart/PaymentBasket.tsx
+++ src/components/Cart/PaymentBasket.tsx
@@ -1,65 +1,166 @@
 import React, {CSSProperties} from "react";
-import { Form, Row, Col, Button } from "react-bootstrap";
+import {Form, useFormik} from "formik";
+import * as Yup from "yup";
+import InputField from "./InputFields";
+import { Link } from "react-router-dom";
 
-export default function PaymentBasket() {
+type PostSchemaType = Record<keyof Form, Yup.AnySchema>
+
+interface Form {
+    firstName: string
+    lastName: string
+    address: string
+    city: string
+    zip: string
+    phoneNumber: string
+    email: string
+}
+
+interface Props {
+    defaultForm?: Form;
+    onSubmit: (form: Form) => void;
+}
+
+const ContactForm = Yup.object().shape({
+    firstName: Yup.string()
+        .min(2, 'To Short')
+        .required('Required'),
+    lastName: Yup.string()
+        .min(2, 'To short')
+        .required('Required'),
+    address: Yup.string()
+        .min(2, 'To Short')
+        .required('Required'),
+    city: Yup.string()
+        .min(2, 'To Short')
+        .required('Required'),
+    zip: Yup.number()
+        .min(2, 'To Short')
+        .required('Required'),
+    phoneNumber: Yup.number()
+        .min(2, 'To Short')
+        .required('Required'),
+    email: Yup.string()
+        .min(2, 'To Short')
+        .required('Required'),
+});
+
+export default function PaymentBasket(props: Props) {
+    const {errors, touched, handleSubmit, handleChange, handleBlur} =
+        useFormik({
+            initialValues: {
+                firstName: '',
+                lastName: '',
+                address: '',
+                city: '',
+                zip: '',
+                phoneNumber: '',
+                email: '',
+            },
+            validationSchema: ContactForm,
+            onSubmit: (form, {resetForm}) => {
+                props.onSubmit(form);
+                resetForm();
+            }
+            })
+
     return (
         <div>
-            <div>
+
                 <p style={cartStyle}>Your contact information</p>
 
-                <Form>
-                    <Row className="mb-3">
-                        <Form.Group as={Col} controlId="formGridName">
-                            <Form.Label>Firstname</Form.Label>
-                            <Form.Control type="name" placeholder="Firstname" />
-                        </Form.Group>
+                        <form onSubmit={handleSubmit}>
+                            <InputField
+                                className="contactInputField"
+                                label="name"
+                                placeholder="Name"
+                                name="name"
+                                onChange={handleChange}
+                                id="firstName"
+                                type="text"
+                                onBlur={handleBlur}
+                                error={touched.firstName && errors.firstName}
+                            />
+                            <InputField
+                                className="contactInputField"
+                                label="lastname"
+                                placeholder="Last Name"
+                                name="lastName"
+                                onChange={handleChange}
+                                id="lastName"
+                                type="text"
+                                onBlur={handleBlur}
+                                error={touched.lastName && errors.lastName}
+                            />
+                            <InputField
+                                className="contactInputField"
+                                label="address"
+                                placeholder="Address"
+                                name="address"
+                                onChange={handleChange}
+                                id="address"
+                                type="text"
+                                onBlur={handleBlur}
+                                error={touched.address && errors.address}
+                            />
+                            <InputField
+                                className="contactInputField"
+                                label="city"
+                                placeholder="City"
+                                name="city"
+                                onChange={handleChange}
+                                id="city"
+                                type="text"
+                                onBlur={handleBlur}
+                                error={touched.city && errors.city}
+                            />
+                            <InputField
+                                className="contactInputField"
+                                label="zip"
+                                placeholder="Zip code"
+                                name="zip"
+                                onChange={handleChange}
+                                id="zip"
+                                type="text"
+                                onBlur={handleBlur}
+                                error={touched.zip && errors.zip}
+                            />
+                            <InputField
+                                className="contactInputField"
+                                label="phoneNumber"
+                                placeholder="Phone Number"
+                                name="phoneNumber"
+                                onChange={handleChange}
+                                id="phoneNumber"
+                                type="text"
+                                onBlur={handleBlur}
+                                error={touched.phoneNumber && errors.phoneNumber}
+                            />
+                            <InputField
+                                className="contactInputField"
+                                label="email"
+                                placeholder="Email"
+                                name="email"
+                                onChange={handleChange}
+                                id="email"
+                                type="text"
+                                onBlur={handleBlur}
+                                error={touched.email && errors.email}
+                            />
+                            <button className="saveBtn" type="submit">Save</button>
 
-                        <Form.Group as={Col} controlId="formGridLastname">
-                            <Form.Label>Lastname</Form.Label>
-                            <Form.Control type="lastname" placeholder="Lastname" />
-                        </Form.Group>
-                    </Row>
+                        </form>
 
-                    <Form.Group className="mb-3" controlId="formGridAddress">
-                        <Form.Label>Address</Form.Label>
-                        <Form.Control placeholder="Address" />
-                    </Form.Group>
 
-                    <Form.Group className="mb-3" controlId="formGridCity">
-                        <Form.Label>City</Form.Label>
-                        <Form.Control placeholder="City" />
-                    </Form.Group>
+                    <p style={cartStyle}>Choice payment</p>
+                    <p style={cartStyle}>Choice shipping</p>
 
-                        <Form.Group as={Col} controlId="formGridZip">
-                            <Form.Label>Zip</Form.Label>
-                            <Form.Control placeholder="Zip" />
-                        </Form.Group>
-
-                    <Row>
-                        <Form.Group className="mb-3" controlId="formGridPhone">
-                            <Form.Label>Phone number</Form.Label>
-                            <Form.Control placeholder="Phone number" />
-                        </Form.Group>
-
-                        <Form.Group className="mb-3" controlId="formGridEmail">
-                            <Form.Label>Email</Form.Label>
-                            <Form.Control placeholder="Email" />
-                        </Form.Group>
-                    </Row>
-
-                    <Button variant="primary" type="submit">
-                        Submit
-                    </Button>
-                </Form>
-
-                <p style={cartStyle}>Choice payment</p>
-                <p style={cartStyle}>Choice shipping</p>
-            </div>
         </div>
     )
 }
 
-const cartStyle:CSSProperties = {
-    fontSize: "2rem",
+    const cartStyle: CSSProperties = {
+        fontSize: "2rem",
+        marginLeft: "2rem"
 
-}
+    }
