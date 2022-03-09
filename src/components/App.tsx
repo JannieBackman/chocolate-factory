9fb6a145924ae9559c4af5cbde3889495c8b46a7
Anna Özmehak <90522443+A-Ozmehak@users.noreply.github.com>
2022-03-09 13:50:24 +0100

    error

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route, BrowserRouter } from "react-router-dom";
+import {Routes, Route, BrowserRouter} from "react-router-dom";
 import Footer from "./Layout/Footer";
 import Header from "./Layout/Header";
 import Main from "./Layout/Main";
@@ -12,33 +12,27 @@ import Checkout from "./Cart/Checkout";
 import DetailsChocolate from "./Products/DetailsChocolate";
 
 function App() {
-  const handleSubmit = () => {};
+    const handleSubmit = () => {
+    };
 
-  return (
-    <div>
-      <Header />
-      <NavigationBar />
+    return (
+        <div>
+            <Header/>
+            <NavigationBar/>
 
-      <Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="/" element={<Header />} />
-        <Route path="/Chocolates" element={<Chocolates />} />
+            <Routes>
+                <Route path="/" element={<Main/>}/>
+                <Route path="/" element={<Header/>}/>
+                <Route path="/Chocolates" element={<Chocolates/>}/>
+                <Route path="/cart" element={<PaymentBasket onSubmit={handleSubmit}/>}/>
+                <Route path="/OrderConfirmation" element={<OrderConfirmation/>}/>
+                <Route path="/CheckOut" element={<Checkout/>}/>
+                <Route path="/DetailsChocolate" element={<DetailsChocolate/>}/>
+            </Routes>
 
-
-                <Routes>
-                    <Route path="/" element={<Main/>}/>
-                    <Route path="/" element={<Header/>}/>
-                    <Route path="/Chocolates" element={<Chocolates />} />
-                    <Route path="/cart" element={<PaymentBasket onSubmit={handleSubmit}/>} />
-                    <Route path="/OrderConfirmation" element={<OrderConfirmation/>} />
-                    <Route path="/CheckOut" element={<Checkout/>} />
-                    <Route path="/DetailsChocolate" element={<DetailsChocolate/>} />
-      </Routes>
-
-
-      <Footer />
-    </div>
-  );
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
