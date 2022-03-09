8bf98b1c3921a2ffb23d4fa8850c10dd8dfd6501
Jannie87 <jannie.backman@gmail.com>
2022-03-09 11:20:02 +0100

    added email

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -3,15 +3,35 @@ import { CSSProperties } from "react";
 function Footer() {
   return (
     <div style={rootStyle}>
-      <h3>Contact us</h3>
+      <div>
+        <h3 style={{ margin: "0" }}>Contact us</h3>
+        <span>
+          Send us an{" "}
+          <a
+            style={{ fontWeight: "bold", color: "white" }}
+            href="mailto:email@example.com"
+          >
+            email!
+          </a>
+        </span>
+      </div>
+
+      <div>
+        <span style={{ display: "flex", alignItems: "flex-end" }}>
+          ©Anna Özmehak, Ella Larsson, Jannie Bäckman Kuurne
+        </span>
+      </div>
     </div>
   );
 }
 
 const rootStyle: CSSProperties = {
   display: "flex",
-  justifyContent: "center",
+  justifyContent: "space-between",
+  alignItems: "flex-end",
   backgroundColor: "#333",
   color: "whitesmoke",
+  padding: "1rem",
+  fontSize: "smaller",
 };
 export default Footer;
