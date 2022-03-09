8be85461ee8670214a2053b6125e877bc8c9a027
Jannie87 <jannie.backman@gmail.com>
2022-03-09 11:36:19 +0100

    added fontfamily to header

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,12 +1,18 @@
 import { CSSProperties } from "react";
 import Cart from "../Cart/cart";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
-    <div style={rootStyle}>
-     <Link style={linkStyle} to="/">Chocolate Factory</Link>
-      <Cart />
+    <div className="header" style={rootStyle}>
+      <div>
+        <Link style={linkStyle} to="/">
+          Chocolate Factory
+        </Link>
+      </div>
+      <div style={cartStyle}>
+        <Cart />
+      </div>
     </div>
   );
 }
@@ -16,13 +22,17 @@ const rootStyle: CSSProperties = {
   justifyContent: "center",
   backgroundColor: "#333",
   color: "whitesmoke",
-    height: "7rem"
+  height: "7rem",
 };
 
-const linkStyle:CSSProperties = {
-    color: "white",
-    fontSize: "3rem",
-    textDecoration: "none",
+const linkStyle: CSSProperties = {
+  color: "white",
+  fontSize: "3rem",
+  textDecoration: "none",
+};
 
-}
+const cartStyle: CSSProperties = {
+  fontSize: "2rem",
+  color: "white",
+};
 export default Header;
