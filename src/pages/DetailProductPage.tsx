f136b192e3aba02daf9832cc3e42f3bb323a3f39
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-03-30 12:30:21 +0200

    add to cart from detailpage

--- src/pages/DetailProductPage.tsx
+++ src/pages/DetailProductPage.tsx
@@ -1,20 +1,29 @@
-import { CSSProperties } from "react";
+import { CSSProperties, useContext } from "react";
 import { FooterDetailPage } from "../components/Layout/Footer";
 import { Product } from "../products";
 import background from "../assets/Background.jpg";
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { CartContext } from "../context/CartContext";
 
 interface Props {
   product: Product;
 }
 
 export default function DetailsChocolate({ product }: Props) {
+  let context = useContext(CartContext);
   return (
     <div style={detailsPage}>
       <h3>{product.title}</h3>
       <img src={product.image} alt="chocolate" style={{ width: "40rem" }} />
       <p>{product.text}</p>
+      <Button
+        style={buttonStyle}
+        variant="dark"
+        onClick={() => context.addToCart(product)}
+      >
+        Add to cart
+      </Button>
       <Link to="/Chocolates">
         <Button variant="dark">Back</Button>
       </Link>
@@ -32,4 +41,8 @@ const detailsPage: CSSProperties = {
   backgroundSize: "cover",
   WebkitBackgroundSize: "cover",
   height: "100vh",
+};
+
+const buttonStyle: CSSProperties = {
+  marginBottom: "1rem",
 };
