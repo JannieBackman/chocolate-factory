f52d863153c2f03bf5d5010bb6c42319c3484cbc
Ella Larsson <91846813+EllaMiri@users.noreply.github.com>
2022-03-31 12:20:45 +0200

    detailpage is working

--- src/pages/DetailProductPage.tsx
+++ src/pages/DetailProductPage.tsx
@@ -1,30 +1,44 @@
-import { CSSProperties, useContext } from "react";
+import { CSSProperties, useContext, useState, useEffect } from "react";
 import { FooterDetailPage } from "../components/Layout/Footer";
-import { Product } from "../products";
 import background from "../assets/Background.jpg";
 import { Button } from "react-bootstrap";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { CartContext } from "../context/CartContext";
+import { mockedProducts, Product } from "../products";
 
-interface Props {
-  product: Product;
-}
-
-export default function DetailsChocolate({ product }: Props) {
+export default function DetailsChocolate() {
   let navigate = useNavigate();
   let context = useContext(CartContext);
+  const { productId } = useParams();
+
+  const [product, setProduct] = useState<Product>();
+
+  useEffect(() => {
+    const getProduct = () => {
+      let foundProduct;
+      if (productId) {
+        foundProduct = mockedProducts.find((p) => p.id === parseInt(productId));
+      }
+
+      setProduct(foundProduct);
+    };
+    getProduct();
+  }, [productId]);
+
   return (
     <div style={detailsPage}>
-      <h3>{product.title}</h3>
-      <img src={product.image} alt="chocolate" style={{ width: "40rem" }} />
-      <p>{product.text}</p>
-      <Button
-        style={buttonStyle}
-        variant="dark"
-        onClick={() => context.addToCart(product)}
-      >
-        Add to cart
-      </Button>
+      <h3>{product?.title}</h3>
+      <img src={product?.image} alt="chocolate" style={{ width: "40rem" }} />
+      <p>{product?.text}</p>
+      {product && (
+        <Button
+          style={buttonStyle}
+          variant="dark"
+          onClick={() => context.addToCart(product)}
+        >
+          Add to cart
+        </Button>
+      )}
       <Button onClick={() => navigate(-1)} variant="dark">
         Back
       </Button>
